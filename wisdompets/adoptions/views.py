2d6f44f74cbb2a8b36d034ab54518ef0e5f98b8f
Jaysonliu <leobest9688@gmail.com>
2021-08-16 22:25:54 +0800

    initial commit

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from .models import Pet
+
+
+def home(request):  # home page => localhost:8000
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets': pets,
+    })
+def pet_detail(request, pet_id):
+    try:
+         pet = Pet.objects.get(id=pet_id) # localhost:8000/adoptions/1/
+    except Pet.DoesNotExist:
+        raise Http404('pet not found')  # 404 page => localhost:8000/adoptions/9999
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })
